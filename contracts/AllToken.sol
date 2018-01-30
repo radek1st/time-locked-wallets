588ec74f472ee0c92a9a7ede51f2e8ddaf39966e
Radek Ostrowski <radek@toptal.com>
2018-01-30 11:38:10 +1100

    updated addresses to deployed contracts

--- contracts/AllToken.sol
+++ contracts/AllToken.sol
@@ -1,7 +1,55 @@
 pragma solidity ^0.4.18;
 
-import './SafeMath.sol';
-import './ERC20.sol';
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+/**
+ * @title ERC20
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 {
+  uint256 public totalSupply;
+
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
 
 /**
  * @title Toptal token
@@ -18,7 +66,7 @@ contract ToptalToken is ERC20 {
   uint8 public decimals = 6;
 
   function ToptalToken() public {
-    totalSupply = 1000000000000;
+  	totalSupply = 1000000000000;
     balances[msg.sender] = totalSupply;
   }
 
@@ -94,4 +142,3 @@ contract ToptalToken is ERC20 {
   event Transfer(address indexed from, address indexed to, uint256 value);
   event Approval(address indexed owner, address indexed spender, uint256 value);
 }
-
